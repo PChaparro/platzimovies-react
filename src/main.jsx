5854a97e6212428da5dcfbf5916774139de6ac6a
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-25 16:38:37 -0500

    :construction: Setup initial routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>Hello world</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<p>Home page...</p>}></Route>
+        <Route path='/trendings' element={<p>Trendings...</p>}></Route>
+        <Route path='/search' element={<p>Search by name...</p>}></Route>
+        <Route path='/category/:name' element={<p>Category...</p>}></Route>
+        <Route path='/movie/:id' element={<p>Movie details...</p>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
