e4c557f116d5445bcbf1251c43c0da15b44c3cb5
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-25 17:59:30 -0500

    :sparkles: Search bar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { FiSearch } from 'react-icons/fi';
+import Styles from './SearchBar.module.css';
+
+export const SearchBar = () => {
+  return (
+    <form className={`${Styles.form} container`}>
+      <label className={Styles.form__label} htmlFor='criteria'>
+        Search a movie:
+      </label>
+      <div className={Styles.form__row}>
+        <input className={Styles.form__input} type='text' name='criteria' id='criteria' />
+        <button className={Styles.form__submit}>
+          <FiSearch color='white' size={'20px'} />
+        </button>
+      </div>
+    </form>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { HomeHero } from '../../components/HomeHero/HomeHero';
+import { SearchBar } from '../../components/SearchBar/SearchBar';
 
 export const Home = () => {
   return (
     <>
       <HomeHero />
+      <SearchBar />
       <p id='categories'>Here</p>
     </>
   );
