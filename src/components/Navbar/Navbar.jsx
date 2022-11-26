e109c00a90c64655fbf5da7c6badf70d6cf8a492
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-11-26 08:00:35 -0500

    :iphone: Navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import { NavLink } from 'react-router-dom';
+import { FiAlignRight } from 'react-icons/fi';
 
 import Styles from './Navbar.module.css';
+import { useRef } from 'react';
 
 export const Navbar = () => {
+  const options = useRef(null);
+
+  const toggleMenuVisitibility = () => {
+    const element = options.current;
+    element.classList.toggle(`${Styles['navbar__options--active']}`);
+
+    element.classList.contains(`${Styles['navbar__options--active']}`)
+      ? element.setAttribute('aria-hidden', false)
+      : element.setAttribute('aria-hidden', true);
+  };
+
   return (
     <nav className={Styles['navbar-container']}>
       <div className={`${Styles.navbar} container`}>
         <NavLink to={'/'}>
           <img className={Styles.navbar__image} src='images/vision-logo.svg' alt='Vision logo' />
         </NavLink>
-        <ul className={Styles.navbar__options}>
+        <ul className={Styles.navbar__options} ref={options}>
           <li className={Styles.navbar__option}>
             <NavLink to={'/trendings'}>Trendings</NavLink>
           </li>
@@ -17,6 +30,11 @@ export const Navbar = () => {
             <NavLink to={'/'}>Saved</NavLink>
           </li>
         </ul>
+        <FiAlignRight
+          size={'24px'}
+          title={'Toggle mobile menu visibility'}
+          onClick={toggleMenuVisitibility}
+        />
       </div>
     </nav>
   );
